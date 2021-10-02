45a53d95087d7dcb061253b7dd3f60658099c110
Amonnuns <ramonwx15@gmail.com>
2021-10-02 15:29:39 -0300

    Filtering only the content searched for

--- scrape.py
+++ scrape.py
@@ -20,35 +20,37 @@ def jumpBetweenPages(lastPageNum,site,carName):
         newpage = newpage+"?o={}&q={}".format(i,carName)
         i+=1
         bsObj = request(newpage)
-        getContent(bsObj)
+        getContent(bsObj, carName)
 
-def getContent(bsObj):
+def getContent(bsObj, carName):
     names = bsObj.find_all("h2",{"class":"sc-1mbetcw-0 fKteoJ sc-ifAKCX jyXVpA"})
     prices = bsObj.find_all("span",{"class":"sc-ifAKCX eoKYee"})
     infos = bsObj.find_all("span",{"class":"sc-1j5op1p-0 lnqdIU sc-ifAKCX eLPYJb"})
     urls = bsObj.find_all("a",{"class":"fnmrjs-0 fyjObc"})
-    listCars(names,prices,infos,urls)
+    listCars(names,prices,infos,urls, carName)
 
-def listCars(names,prices,infos,urls):
+def listCars(names,prices,infos,urls,searchedCar):
     listOfCars = []
     for name, price, info, url in zip(names,prices,infos,urls):
-        urlhref = url['href']
-        carName = name.get_text()
-        carPrice = price.get_text()
-        exchange = ""
+        if searchedCar in name.get_text().lower():
+            urlhref = url['href']
+            carName = name.get_text()
+            carPrice = price.get_text()
+            exchangeInfo = ""
+        else:
+            continue
         try:
             mileage, exchangeInfo, gasType = info.get_text().split("|")
         except:
             try:
                 mileage, exchangeInfo = info.get_text().split("|")
-                exchange = exchangeInfo.split(": ")[1]
+                exchangeInfo = exchangeInfo.split(": ")[1]
             except:
                 mileage = info.get_text()
 
-        listOfCars.append(Car(carName, carPrice, mileage, exchange, urlhref))
-
-        print(f"Name of the car:{carName}\nPrice:{carPrice}\nMileage:{mileage}\nExchange:{exchange}\nUrl:{urlhref}\n --------------------------------------------------------------")
-
+        listOfCars.append(Car(carName, carPrice, mileage, exchangeInfo, urlhref))
+        
+        print(f"Name of the car:{carName}\nPrice:{carPrice}\nMileage:{mileage}\nExchange:{exchangeInfo}\nUrl:{urlhref}\n --------------------------------------------------------------")
 
 def request(site):
     hdr = {'User-Agent':'Mozilla/5.0'}
@@ -65,7 +67,7 @@ def main():
     lastPageNum = bsObj.find("a",{"class":"sc-1bofr6e-0 iRQkdN","data-lurker-detail":"last_page"})
     lastPageNum = re.findall(r'\d+', lastPageNum['href'])
 
-    getContent(bsObj)
+    getContent(bsObj,carName)
     jumpBetweenPages(lastPageNum,site,carName)
 
 
